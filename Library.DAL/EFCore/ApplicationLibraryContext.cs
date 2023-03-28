c7315efc243f12fa385ed7bd1f4e17145586bf02
AlKapsh <kapshul.al@gmail.com>
2023-03-28 14:07:33 +0300

    Add Application Context

---
+++ Library.DAL/EFCore/ApplicationLibraryContext.cs
@@ -0,0 +1,35 @@
+﻿using Library.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.DAL.EFCore
+{
+    internal class ApplicationLibraryContext: DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Issuance> Issuances { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<Reader> Readers { get; set; }
+
+
+        public ApplicationLibraryContext(DbContextOptions options): base(options)
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server= (localdb)\\mssqllocaldb;Database=LibraryDB;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
