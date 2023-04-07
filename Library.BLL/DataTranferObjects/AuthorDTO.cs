f3817018316ac3c0caa6f2d71c20668b38fbcc49
AlKapsh <kapshul.al@gmail.com>
2023-04-07 08:09:13 +0300

    Added DTO classes and Mapping Profile class

---
+++ Library.BLL/DataTranferObjects/AuthorDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.BAL.DataTranferObjects
+{
+    public class AuthorDTO
+    {
+        public string FistName { get; set; }
+        public string SecondName { get; set; }
+    }
+}
---
+++ Library.BLL/Extensions/MappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Library.BAL.DataTranferObjects;
+using Library.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.BAL.Extensions
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() 
+        {
+            CreateMap<AuthorDTO, Author>().ReverseMap();
+            CreateMap<IssuanceDTO, Issuance>().ReverseMap();
+            CreateMap<BookDTO, Book>().ReverseMap();
+            CreateMap<PublisherDTO, Publisher>().ReverseMap();
+            CreateMap<ReaderDTO, Reader>().ReverseMap();
+        }
+    }
+}
